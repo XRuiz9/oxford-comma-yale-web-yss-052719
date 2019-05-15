f082964af65f3a0ebc8b9ee6c8e3faf6099b25e0
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-15 05:42:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
-  if (array.count == 1)
-    array.pop
-  end
 
   if (array.count == 2)
     array.join(" and ")
   end
+
+  if (array.count == 1)
+    array.pop
+  end
+
 end
