7c25f095751340f93b741735ea255ac7f9459087
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-15 05:45:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
 
-  if (array.count == 2)
+  if (array.count > 2)
+    last = array.pop
+    printLine = array.join(", ")
+  elsif (array.count == 2)
     array.join(" and ")
   else
     array.pop
